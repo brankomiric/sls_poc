67892c1960ad6a5d948d40b4a6533a51e73819a5
cr33pyfkntweekR <captainwhiteboii@gmail.com>
2023-10-24 21:58:50 +0200

    validating input

--- golang_poc/ip-addresser/main.go
+++ golang_poc/ip-addresser/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"golang_poc/common"
 )
 
 type response struct {
@@ -15,8 +16,16 @@ type response struct {
 
 func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	sourceIp := req.Headers["x-forwarded-for"]
-	ipAddress := &response{sourceIp}
-	body, err := json.Marshal(ipAddress)
+
+	var body []byte
+	var err error
+	if common.IsValidIPAddress(sourceIp, "v4") || common.IsValidIPAddress(sourceIp, "v6") {
+		ipAddress := &response{sourceIp}
+		body, err = json.Marshal(ipAddress)
+	} else {
+		body, err = json.Marshal(map[string]string{"message": "Invalid IP Address"})
+	}
+
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
